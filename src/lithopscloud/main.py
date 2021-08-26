d6f8715cfeba5617987216fc4cb6f31d944ba2e2
kpavel <kpavel@il.ibm.com>
2021-08-26 12:07:40 +0300

    multiple bug fixes. added --version

--- src/lithopscloud/main.py
+++ src/lithopscloud/main.py
@@ -1,12 +1,15 @@
 import importlib
+import pkg_resources
 import yaml
 import tempfile
 import click
+import os
 
 def get_base_config(input_file, format):
     
     if not input_file:
-        input_file = f'./src/lithopscloud/modules/{format}/defaults.yaml'
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        input_file = f'{dir_path}/modules/{format}/defaults.yaml'
 
     with open(input_file) as f:
         template_config = yaml.safe_load(f)
@@ -16,9 +19,23 @@ def get_base_config(input_file, format):
 @click.command()
 @click.option('--output-file', '-o', help='Output filename to save configurations')
 @click.option('--input-file', '-i', help=f'Template for the new configuration')
-@click.option('--iam-api-key', required=True, help='IAM_API_KEY')
-@click.option('--format', type=click.Choice(['lithops', 'ray']), required=True, help='format of the output file')
-def builder(iam_api_key, output_file, input_file, format):
+@click.option('--iam-api-key', help='IAM_API_KEY')
+@click.option('--format', type=click.Choice(['lithops', 'ray']), help='format of the output file')
+@click.option('--version', '-v', help=f'Get package version', is_flag=True)
+def builder(iam_api_key, output_file, input_file, format, version):
+
+    if version:
+        print(f"{pkg_resources.get_distribution('lithopscloud').project_name} {pkg_resources.get_distribution('lithopscloud').version}")
+        exit(0)
+
+    if not iam_api_key:
+        print(f"Error: Missing option '--iam-api-key'")
+        exit(1)
+
+    if not format:
+        print(f"Error: Missing option 'format'")
+        exit(1)
+
     print(f"\n\033[92mWelcome to vpc config export helper\033[0m\n")
 
     base_config = get_base_config(input_file, format)
