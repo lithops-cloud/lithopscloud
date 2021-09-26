b74966b59fdeebe5778845bcff165341c86d5640
kpavel <kpavel@il.ibm.com>
2021-09-26 10:21:03 +0300

    removed ssh_key_filename from gen2 lithops config

--- src/lithopscloud/modules/gen2/lithops/ssh_key.py
+++ src/lithopscloud/modules/gen2/lithops/ssh_key.py
@@ -14,5 +14,5 @@ class LithopsSshKeyConfig(SshKeyConfig):
 
     def update_config(self, ssh_key_id, ssh_key_path, ssh_user):
         self.base_config['ibm_vpc']['key_id'] = ssh_key_id
-        self.base_config['ibm_vpc']['ssh_key_filename'] = ssh_key_path
+#        self.base_config['ibm_vpc']['ssh_key_filename'] = ssh_key_path
         self.base_config['ibm_vpc']['ssh_user'] = ssh_user
