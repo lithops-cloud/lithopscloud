f82c9699e6f4e291c6e3abc0069394ff8914ffed
kpavel <kpavel@il.ibm.com>
2021-11-30 09:25:35 +0200

    bug fix

--- src/lithopscloud/modules/gen2/image.py
+++ src/lithopscloud/modules/gen2/image.py
@@ -17,7 +17,7 @@ class ImageConfig(ConfigBuilder):
 
         image_objects = get_image_objects()
 
-        default = find_default(self.defaults, image_objects, id='image_id') or 'ibm-ubuntu-20-04-minimal-amd64-2'
+        default = find_default(self.defaults, image_objects, id='image_id') or 'ibm-ubuntu-20-04-2-minimal-amd64-1'
         image_obj = find_obj(image_objects, 'Please choose \033[92mUbuntu\033[0m 20.04 VM image, currently only Ubuntu supported', default=default)
 
         return image_obj['id'], image_obj['minimum_provisioned_size']
--- src/lithopscloud/modules/gen2/ray/image.py
+++ src/lithopscloud/modules/gen2/ray/image.py
@@ -7,5 +7,7 @@ class RayImageConfig(ImageConfig):
         if self.base_config.get('available_node_types'):
             for available_node_type in self.base_config['available_node_types']:
                 self.base_config['available_node_types'][available_node_type]['node_config']['image_id'] = image_id
+                self.base_config['available_node_types'][available_node_type]['node_config']['boot_volume_capacity'] = minimum_provisioned_size
         else:
             self.base_config['available_node_types']['ray_head_default']['node_config']['image_id'] = image_id
+            self.base_config['available_node_types']['ray_head_default']['node_config']['boot_volume_capacity'] = minimum_provisioned_size
