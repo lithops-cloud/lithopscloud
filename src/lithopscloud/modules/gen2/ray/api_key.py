4f7c4e69ff2fc4978df5fed57d4c35cbae5eb2f1
kpavel <kpavel@il.ibm.com>
2022-04-19 01:15:53 +0300

    wip

--- src/lithopscloud/modules/gen2/ray/api_key.py
+++ src/lithopscloud/modules/gen2/ray/api_key.py
@@ -13,6 +13,7 @@ class RayApiKeyConfig(ApiKeyConfig):
 
     def update_config(self, iam_api_key, compute_iam_endpoint=None, cos_iam_api_key=None):
         self.base_config['provider']['iam_api_key'] = iam_api_key
+        self.base_config['provider']['iam_endpoint'] = compute_iam_endpoint
         self.base_config.pop('ibm', None)
         return self.base_config
 
